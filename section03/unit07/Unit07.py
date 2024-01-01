c1bf78d855c6957e476bb6f92f480965343ab057
calebeLAR <ca6ele@gmail.com>
2024-01-01 18:31:51 -0300

    trybe.BootCampAWS.section03.unit07.js

---
+++ section03/unit07/Unit07.py
@@ -0,0 +1,7 @@
+class Unit07:
+    title = "Referências e recursos adicionais"
+    topics = [
+        "Referências e recursos adicionais",
+        "Referências",
+        "Recursos adicionais",
+    ]
